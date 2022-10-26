b5ac40a58e0bbd2522b0fda0e6d765095375a960
EvatsugDnalloR <gustaverolland777@gmail.com>
2022-10-26 22:21:53 +0200

    Second Function Started

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,13 @@ public class Main {
         contacts.add("LI");
         contacts.add("AYM");
 
+        Scanner universalScanner = new Scanner(System.in);
         for (int i = 0; i<999999999; i++) {
-            Scanner universalScanner = new Scanner(System.in);
 
             System.out.println("CHOOSE WHAT YOU'D LIKE TO DO");
             System.out.println("1. All Contacts");
             System.out.println("2. All Messages");
-            System.out.println("3. quit the APP");
+            System.out.println("3. Quit the APP");
 
             int input1 = universalScanner.nextInt();
             if (input1==1) {
@@ -28,10 +28,11 @@ public class Main {
                         System.out.println(contacts.get(p));
                     }
                     System.out.println("CHOOSE YOUR ACTION");
-                    System.out.println("1. Add New Contacts");
-                    System.out.println("2. Delete Contacts");
-                    System.out.println("3. Modify Contacts");
-                    System.out.println("4. Go Back To Last Page");
+                    System.out.println("1. Add New Contact");
+                    System.out.println("2. Delete a Contact");
+                    System.out.println("3. Modify a Contact");
+                    System.out.println("4. Search a Contact");
+                    System.out.println("5. Go Back To The Menu");
 
                     int input11 = universalScanner.nextInt();
                     if (input11==1) {
@@ -66,13 +67,36 @@ public class Main {
                         } else if (!contacts.contains(originalName)) {
                             System.out.println(originalName + " doesn't exist...");
                         }
+                    } else if (input11==4) {
+                        System.out.println("Enter the name you want to search...");
+                        String searchName = universalScanner.next();
+                        if (contacts.contains(searchName)) {
+                            System.out.println(searchName + " does exist...");
+                        } else if (!contacts.contains(searchName)) {
+                            System.out.println(searchName + " doesn't exist...");
+                        }
                     } else {
                         break;
                     }
                 }
+            } else if (input1==2) {
+                for (int t = 0; t<999999999; t++) {
+                    System.out.println("CHOOSE YOUR ACTION");
+                    System.out.println("1. Show the Messages");
+                    System.out.println("2. Send new Messages");
+                    System.out.println("3. Go Back To The Menu");
 
+                    int input12 = universalScanner.nextInt();
+                    if (input12==1) {
+                        System.out.println("Choose the Messages from which contacts you want to check");
+                        for (int p=0; p<contacts.size(); p++) {
+                            System.out.println("Message from: " + contacts.get(p));
+                        }
+                        String contactsMessage = universalScanner.next();
 
+                    }
+                }
             }
-    }
+        }
 }
 }
